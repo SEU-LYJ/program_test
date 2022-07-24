ba74537d7a35a9da0c9b6d1e48e2e1e7706666e9
liuyingjie <704232442@qq.com>
2022-07-24 19:57:01 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
-
-bool is_prime(long input)
-{
-    if(input <= 1) {
-        return false;
-    }
-
-    for(int i = 2; i * i <= input; ++i) {
-        if(input % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
+#include <set>
 using namespace std;
+
 int main(void) {
     #ifdef DEBUG_TEST_LYJ
     freopen("input.txt","r", stdin);
     #endif
-    int max_number;
-    cin >> max_number;
-    for(int i = 2; i <= max_number; ++i) {
-        if(is_prime(i)) {
-            cout << i << " ";
+    string input;
+    getline(cin, input);
+    int sum = 0;
+    for(auto tmp = input.begin(); tmp < input.end(); ++tmp)
+    {
+        if(isupper(*tmp)) {
+            ++sum;
         }
     }
-    cout << endl;
+    cout << sum << endl;
     return 0;
-}
+} 
