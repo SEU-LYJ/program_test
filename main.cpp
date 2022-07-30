de0259ffcfcec6f52b0df7baa466191e6fec159e
liuyingjie <704232442@qq.com>
2022-07-30 16:35:14 +0800

    next permutation

--- main.cpp
+++ main.cpp
@@ -15,28 +15,22 @@
 
 using namespace std;
 
+struct data_t {
+    int len;
+    int a_end;
+    int b_end;
+};
+
 int main(void)
 {
 #ifdef DEBUG_TEST_LYJ
     freopen("input.txt", "r", stdin);
 #endif
- printf("%d\r\n", 12345678900);
- printf("%u\r\n", 12345678900); //无符号
- printf("%ld\r\n", 12345678900);
- printf("%lu\r\n", 12345678900);
- printf("%5d\r\n", 1234); //位数为3，不足左边补空格
- printf("%05d\r\n", 1234); //位数为3，不足左边补0
- printf("%o\r\n", 12); //无符号8进制
- printf("%x\r\n", 129); //无符号16进制
- printf("%X\r\n", 129); //无符号16进制大写
- printf("%04x\r\n", 129); // 位数为4，不足补零
- printf("%f\r\n", 12.345); // float
- printf("%lf\r\n", 12.3456); // double
- printf("%8.2f\r\n", 12.345); // 指定小数位数,不足左端补空格,5为包含小数的总列数
- printf("%-8.2f\r\n", 12.345); // 指定小数位数,不足右端补空格
- printf("%08.2f\r\n", 12.345); // 指定小数位数,不足左端补0
- printf("%08s\r\n", "123"); // 字符串,不足左端补0
- printf("%8s\r\n", "123"); // 字符串,不足左端补空格
- printf("%8p\r\n", 123); // 指针,不足左端补空格
+    vector<string> m_test{"abc", "def", "ghi"};
+    sort(m_test.begin(), m_test.end());
+    cout << m_test[0] << " " << m_test[1] << " " << m_test[2] << endl;
+    while(next_permutation(m_test.begin(), m_test.end())) {
+        cout << m_test[0] << " " << m_test[1] << " " << m_test[2] << endl;
+    }
     return 0;
 }
